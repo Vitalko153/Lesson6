a0ac560675758cf5b0324724bb8c747713a82122
DonVitalko <DonVitalko@mail.ru>
2021-03-04 23:22:15 +0300

    Домашние задание задание номер шесть.

--- src/Animal.java
+++ src/Animal.java
@@ -1,3 +1,11 @@
 public class Animal {
 
+    static int count;
+
+
+    public static void score(){
+        System.out.println("Создано " + count + " животных");
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,15 @@ public class Main {
         int lengthBarrier = 300;
         int lengthPull = 5;
         Cat barsic = new Cat("Барсик");
+        Animal.count++;
         Dog bobik = new Dog("Бобик");
-
+        Animal.count++;
         barsic.run(lengthBarrier);
         bobik.run(lengthBarrier);
         barsic.swim(lengthPull);
         bobik.swim(lengthPull);
+        Animal.score();
+
 
 
     }
